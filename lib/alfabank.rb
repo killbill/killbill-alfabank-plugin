e4a46f396c23821076574906a8340c379e1237bf
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-04-22 09:22:37 -0700

    Update to latest generated code
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/alfabank.rb
+++ lib/alfabank.rb
@@ -5,6 +5,7 @@ require 'active_merchant'
 require 'active_support'
 require 'bigdecimal'
 require 'money'
+require 'monetize'
 require 'pathname'
 require 'sinatra'
 require 'singleton'
@@ -20,8 +21,3 @@ require 'alfabank/models/payment_method'
 require 'alfabank/models/response'
 require 'alfabank/models/transaction'
 
-class Object
-  def blank?
-    respond_to?(:empty?) ? empty? : !self
-  end
-end
